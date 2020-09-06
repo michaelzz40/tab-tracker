b2aeb21c62ff265578c9255b1ca09d97d1d8faa0
michaelzz40 <61964609+michaelzz40@users.noreply.github.com>
2020-09-06 14:30:09 +0800

    Added server

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,19 +1,13 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "commonjs": true,
+        "es2021": true,
+        "node": true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/essential"
-    ],
     "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
+        "ecmaVersion": 12
     },
-    "plugins": [
-        "vue"
-    ],
     "rules": {
     }
 };
